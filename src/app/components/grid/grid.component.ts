4451068b7890c32fd0673e24fb56c06ed9ef06a7
marharita08 <rozgon.margarita08@gmail.com>
2023-07-26 16:25:32 +0300

    add password service, add input component

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
-import {defaultColor} from "../form/form.component";
+import {Component, OnInit} from '@angular/core';
+import {IGridState} from "../../interfaces/grid.state";
+import {defaultState} from "../../services/password.service";
+import {PasswordService} from "../../services/password.service";
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css']
 })
-export class GridComponent {
-  @Input() block1Color: string = defaultColor;
-  @Input() block2Color: string = defaultColor;
-  @Input() block3Color: string = defaultColor;
+export class GridComponent implements OnInit {
+  gridState: IGridState = defaultState;
+
+  constructor(private passwordService:PasswordService) {
+  }
+
+  ngOnInit() {
+    this.gridState = this.passwordService.getGridState();
+  }
 }
